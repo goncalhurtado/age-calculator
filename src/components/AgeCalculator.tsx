5d21e79538c38dd0a98ffa1f473d651679601974
goncalhurtado <gonzalomilano@gmail.com>
2024-01-16 20:31:23 -0300

    added style to input comp

--- src/components/AgeCalculator.tsx
+++ src/components/AgeCalculator.tsx
@@ -1,3 +1,10 @@
+import Input from "./Input";
+
 export const AgeCalculator = () => {
-  return <div className="ageCalculator">AgeCalculator</div>;
+  return (
+    <div className="ageCalculator">
+      AgeCalculator
+      <Input />
+    </div>
+  );
 };
