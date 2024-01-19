e8378568cabc572721aa25e0e386ef21c45c4a0b
goncalhurtado <gonzalomilano@gmail.com>
2024-01-19 18:41:11 -0300

    added footer and links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,26 @@
 const Footer = () => {
-  return <div className="footer"></div>;
+  return (
+    <div className="footer">
+      <p>
+        Develop by Goncal Hurtado{" "}
+        <a href="https://github.com/goncalhurtado">
+          {" "}
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+            alt="github icon"
+            style={{ width: "20px" }}
+          />
+        </a>{" "}
+        <a href="https://www.linkedin.com/in/goncalhurtado/">
+          <img
+            src="https://cdn-icons-png.flaticon.com/256/174/174857.png"
+            alt="linkedin icon"
+            style={{ width: "20px" }}
+          />
+        </a>
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
